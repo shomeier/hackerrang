89518e90a0a327e6e340c7bd68a560d13a18125e
Sascha Homeier <sascha.homeier@gmail.com>
2021-07-21 13:34:30 +0700

    Shell - World of Numbers

---
+++ linux-shell/src/world-of-numbers/world-of-numbers.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem
+
+read a
+read b
+echo $((a + b))
+echo $((a - b))
+echo $((a * b))
+echo $((a / b))
