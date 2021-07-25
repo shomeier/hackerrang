d8012410cc87d893346818ea6b8e32314617530a
Sascha Homeier <sascha.homeier@gmail.com>
2021-07-25 15:57:45 +0700

    Using parameterized test

--- thirty-days-of-code/java/src/test/java/org/sho/day23/bstlevelordertraversal/SolutionTest.java
+++ thirty-days-of-code/java/src/test/java/org/sho/day23/bstlevelordertraversal/SolutionTest.java
@@ -2,22 +2,23 @@ package org.sho.day23.bstlevelordertraversal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SolutionTest extends AbstractSolutionTest {
 
-    @Test
-    void testOne() {
-        assertEquals(List.of(3, 2, 5, 1, 4, 7), Solution.getLevelOrder(buildBst(List.of(3, 5, 4, 7, 2, 1))));
+    @ParameterizedTest
+    @MethodSource("provideData")
+    void testOne(List<Integer> expected, List<Integer> input) {
+        assertEquals(expected, Solution.getLevelOrder(buildBst(input)));
     }
 
-    @Test
-    void testTwo() {
-        assertEquals(List.of(4, 2, 6, 1, 3, 5, 7), Solution.getLevelOrder(buildBst(List.of(4, 2, 1, 3, 6, 5, 7))));
-    }
-
-    @Test
-    void testThree() {
-        assertEquals(List.of(5, 2, 6, 3, 4), Solution.getLevelOrder(buildBst(List.of(5, 2, 3, 4, 6))));
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of(List.of(3, 2, 5, 1, 4, 7), List.of(3, 5, 4, 7, 2, 1)),
+                Arguments.of(List.of(4, 2, 6, 1, 3, 5, 7), List.of(4, 2, 1, 3, 6, 5, 7)),
+                Arguments.of(List.of(5, 2, 6, 3, 4), List.of(5, 2, 3, 4, 6)));
     }
 }
