705000db51660e4ea837ceb09c6063d17b743dba
Sascha Homeier <sascha.homeier@gmail.com>
2021-07-25 16:54:32 +0700

    Shell - Adjusted looping-and-skipping

--- linux-shell/src/looping-and-skipping/looping-and-skipping.sh
+++ linux-shell/src/looping-and-skipping/looping-and-skipping.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
+echo "First loop ..."
 # this form of loop only works in bash, not in zsh
-for i in {1..99..2}
-do
+for i in {1..99..2}; do
     echo "$i"
 done
 
+echo "Second loop ..."
 # this form of loop works in both: bash and zsh
-for (( i=1; i <= 99; i++ ))
-do
+for ((i = 1; i <= 99; i += 2)); do
     echo "$i"
 done
