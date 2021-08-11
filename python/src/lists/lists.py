99ece637d5919cae849c7c2ba5e086c97d01d59d
Sascha Homeier <sascha.homeier@gmail.com>
2021-08-11 20:02:56 +0700

    Python - Lists - renamed variable

--- python/src/lists/lists.py
+++ python/src/lists/lists.py
@@ -1,22 +1,22 @@
 def solution():
     N = int(input())
-    list = []
+    _list = []
     for _ in range(N):
         cmd, *args = input().split()
         if cmd == 'insert':
-            list.insert(int(args[0]), int(args[1]))
+            _list.insert(int(args[0]), int(args[1]))
         elif cmd == 'print':
-            print(list)
+            print(_list)
         elif cmd == 'remove':
-            list.remove(int(args[0]))
+            _list.remove(int(args[0]))
         elif cmd == 'append':
-            list.append(int(args[0]))
+            _list.append(int(args[0]))
         elif cmd == 'sort':
-            list.sort()
+            _list.sort()
         elif cmd == 'pop':
-            list.pop()
+            _list.pop()
         elif cmd == 'reverse':
-            list.reverse()
+            _list.reverse()
 
 
 if __name__ == '__main__':
