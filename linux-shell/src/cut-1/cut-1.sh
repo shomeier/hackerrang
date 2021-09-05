5bbf15e579a5c6fbbeb969c530a24944ed67beb2
Sascha Homeier <sascha.homeier@gmail.com>
2021-09-05 20:42:40 +0700

    Shell - Cut 1

---
+++ linux-shell/src/cut-1/cut-1.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+# https://www.hackerrank.com/challenges/text-processing-cut-1/problem
+
+# this solution works:
+# while IFS= read -r line; do
+#     echo "$line" | cut -c 3
+# done
+
+# this solution also works:
+cut -c 3
