ef9363fd4adb16c9a9ae4e47ea8b1d20c6fb78b7
Sascha Homeier <sascha.homeier@gmail.com>
2021-09-11 10:02:32 +0700

    Python - Mutations

---
+++ python/src/mutations/mutations-with-list.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/python-mutations/problem
+
+def mutate_string(string, position, character):
+    str_list = list(string)
+    str_list[position] = character
+    return ''.join(str_list)
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
