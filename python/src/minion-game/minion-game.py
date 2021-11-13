c7042bc32afb22aee944495e7bbaa97725589607
Sascha Homeier <sascha.homeier@gmail.com>
2021-11-13 11:27:44 +0700

    Python - Minion Game - Improvement

--- python/src/minion-game/minion-game.py
+++ python/src/minion-game/minion-game.py
@@ -2,18 +2,21 @@
 
 def minion_game(string):
     vowels = 'AEIOU'
-    all_strings = [string[i: j] for i in range(len(string))
-                   for j in range(i + 1, len(string) + 1)]
-    stuarts_words = list(filter(lambda s: not s[0] in vowels, all_strings))
-    kevins_words = list(filter(lambda s: s[0] in vowels, all_strings))
-    if len(stuarts_words) > len(kevins_words):
-        print("Stuart", len(stuarts_words))
-    elif len(kevins_words) > len(stuarts_words):
-        print("Kevin", len(kevins_words))
+    length = len(string)
+    stuarts_words, kevins_words = 0,0
+    for i in range(0, len(string)):
+        if string[i] in vowels:
+            kevins_words += length - i
+        else:
+            stuarts_words += length - i
+    if stuarts_words > kevins_words:
+        print("Stuart", stuarts_words)
+    elif kevins_words > stuarts_words:
+        print("Kevin", kevins_words)
     else:
         print("Draw")
 
 
 if __name__ == '__main__':
-    s = input()
+    s = input().strip()
     minion_game(s)
