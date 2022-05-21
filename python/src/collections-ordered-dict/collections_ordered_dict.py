3842233d991af2949fe7f620b28503bfdb8831bc
Sascha Homeier <sascha.homeier@gmail.com>
2022-05-21 17:13:54 +0700

    Python - Ordered Collections
    Using multi assignment

--- python/src/collections-ordered-dict/collections_ordered_dict.py
+++ python/src/collections-ordered-dict/collections_ordered_dict.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
     ordered_dictionary = OrderedDict()
     for i in range(n):
         data = input().strip().split()
-        name = " ".join(data[:-1])
-        price = int(data[-1])
+        name, price = " ".join(data[:-1]), int(data[-1])
         ordered_dictionary[name] = ordered_dictionary.get(name, 0) + int(price)
     for key in ordered_dictionary:
         print(key, ordered_dictionary.get(key))
