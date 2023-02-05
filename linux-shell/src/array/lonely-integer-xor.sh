9e80f35bdfab09051246c0c7bef5784bbe764a94
Sascha Homeier <sascha.homeier@gmail.com>
2023-02-05 16:55:00 +0700

    Shell - Added link to lonely integer XOR solution

--- linux-shell/src/array/lonely-integer-xor.sh
+++ linux-shell/src/array/lonely-integer-xor.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # https://www.hackerrank.com/rest/contests/master/challenges/lonely-integer-2/hackers/fR0DDY/download_solution?primary=true
+# for explanation see: https://oliverjumpertz.com/xor-bitwise-xor-and-using-it-to-solve-an-algorithm-challenge/
 
 read N
 result=0
